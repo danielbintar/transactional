79030ea5d083ef758ec806633b31e1d6904a82d4
danielbintar <daniel_bintar@yahoo.com>
2021-03-27 13:56:17 +0700

    remove log (#1)

--- pkg/publisher_test.go
+++ pkg/publisher_test.go
@@ -5,49 +5,38 @@ import (
 	"errors"
 	"testing"
 
-	mockInternal "github.com/danielbintar/transactional/internal/mocks"
 	mockPkg "github.com/danielbintar/transactional/pkg/mocks"
 	"github.com/danielbintar/transactional/pkg"
 
 	"github.com/golang/mock/gomock"
 
-	"go.uber.org/zap"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestPublisherWithSkipLockedPublish(t *testing.T) {
+func TestKafkaPublisherPublish(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	log := mockInternal.NewMockLogZap(ctrl)
-	metric := mockInternal.NewMockMetric(ctrl)
 	db := mockPkg.NewMockDB(ctrl)
 
-	publisher := pkg.NewPublisherWithSkipLocked(log, metric)
+	publisher := pkg.NewKafkaPublisher()
 	ctx := context.Background()
 	expectedQuery := "INSERT INTO transactional_events (topic, payload) VALUES(?,?)"
 
 	t.Run("success", func(t *testing.T) {
 		db.EXPECT().ExecContext(ctx, expectedQuery, "topic", "payload")
-		metric.EXPECT().Timing("backend_latency", gomock.Any(), []string{"category:sql", "sub:publish", "status:ok"}, float64(1))
-		log.EXPECT().Info("Success publish event",
-			zap.Strings("tags", []string{"sql", "publish"}),
-			zap.String("request_id", "no-request-id"),
-			gomock.Any(),
-		)
 
-		publisher.Publish(ctx, db, "topic", "payload")
+		err := publisher.Publish(ctx, db, "topic", "payload")
+
+		assert.Equal(t, nil, err, "publish success should not return error")
 	})
 
 	t.Run("failed", func(t *testing.T) {
-		db.EXPECT().ExecContext(ctx, expectedQuery, "topic", "payload").Return(nil, errors.New("connection refused"))
-		metric.EXPECT().Timing("backend_latency", gomock.Any(), []string{"category:sql", "sub:publish", "status:fail"}, float64(1))
-		log.EXPECT().Info("Failed publish event",
-			zap.String("error", "connection refused"),
-			zap.Strings("tags", []string{"sql", "publish"}),
-			zap.String("request_id", "no-request-id"),
-			gomock.Any(),
-		)
+		msg := errors.New("connection refused")
+		db.EXPECT().ExecContext(ctx, expectedQuery, "topic", "payload").Return(nil, msg)
 
-		publisher.Publish(ctx, db, "topic", "payload")
+		err := publisher.Publish(ctx, db, "topic", "payload")
+
+		assert.Equal(t, msg, err, "publish failed should return error")
 	})
 }
